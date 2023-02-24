9f00c7dfb853ce73e02812624dd1c015660b3c40
diejofre <diego-jofre@live.com.ar>
2023-02-24 11:56:29 -0300

    agregué una navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
       <h1>My App</h1>
       <h2>Aplicación de ejemplo</h2>
     </>
